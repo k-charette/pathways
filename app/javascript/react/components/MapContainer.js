6c752f4ed89b650e22f8f3e7517753cbb11503a2
Ken Charette <charettekenneth@gmail.com>
2019-11-04 14:30:32 -0500

    Small style change

--- app/javascript/react/components/MapContainer.js
+++ app/javascript/react/components/MapContainer.js
@@ -8,25 +8,23 @@ const MapContainer = props => {
     width: "70%",
     height: "70%"
   }
-
-
   return (
-      <Map
-        google={props.google}
-        zoom={13}
-        style={mapStyles}
-        initialCenter={{ lat: 42.358119, lng: -71.057655 }}
-      >
-        <Marker
-          title={'The station with the most bikes.'}
-          name={'South Station'}
-          position={{ lat: 42.359608, lng: -71.059479 }}
-        />
-        <InfoWindow> </InfoWindow>
-      </Map>
+    <Map
+      google={props.google}
+      zoom={13}
+      style={mapStyles}
+      initialCenter={{ lat: 42.358119, lng: -71.057655 }}
+    >
+      <Marker
+        title={'The station with the most bikes.'}
+        name={'South Station'}
+        position={{ lat: 42.359608, lng: -71.059479 }}
+      />
+      <InfoWindow> </InfoWindow>
+    </Map>
   )
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAKhT2ZbfEWEBLSouEn79BAmCJngeQO8B4'
+  apiKey: ''
 })(MapContainer);
