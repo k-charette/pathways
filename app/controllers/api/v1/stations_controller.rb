26647cc7476316fe1944f1769d3466f602e41622
Ken Charette <charettekenneth@gmail.com>
2019-12-02 17:38:08 -0500

    Small changes in SearchIndexContainer

--- app/controllers/api/v1/stations_controller.rb
+++ app/controllers/api/v1/stations_controller.rb
@@ -2,17 +2,6 @@ class Api::V1::StationsController < ApplicationController
   protect_from_forgery unless: -> { request.format.json? }
 
   def index
-    if params["/stations"]
-      search_results = Station.near(params["/stations"]["search"], 1)
-      search_results = search_results.first.id
-    end
-
-    if search_results
-      render json: {search_results: search_results}
-    else
-      render json: Station.all
-    end
-
     url = "https://gbfs.bluebikes.com/gbfs/en/station_information.json"
     response = Faraday.get("#{url}")
     parsed_response = JSON.parse(response.body)
@@ -25,6 +14,17 @@ class Api::V1::StationsController < ApplicationController
       lat = station["lat"]
       lon = station["lon"]
       capacity = station["capacity"]
+    end
+    
+    if params["/stations"]
+      search_results = Station.near(params["/stations"]["search"], 1)
+      search_results = search_results.first.id
+    end
+
+    if search_results
+      render json: search_results
+    else
+      render json: Station.all
     end
   end
 
